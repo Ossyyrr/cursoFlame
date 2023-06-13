20b5993a0ebd30540dd1b4254c064a83fbbc19b1
Patri <patricia.manzanero-jimenez@capgemini.com>
2023-06-13 16:52:46 +0200

    wip

---
+++ pruebasflame/lib/components/circle_position_component.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+import 'dart:ui';
+import 'dart:async';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
+
+class CirclePositionComponent extends PositionComponent with CollisionCallbacks {
+  static const int circleSpeed = 250;
+  static const double circleWidth = 100, circleHeigth = 100;
+  late int circleDirectionX;
+  late int circleDirectionY;
+
+  Random random = Random();
+
+  late double screenWidth, screenHeight;
+  final ShapeHitbox hitBox = CircleHitbox();
+
+  @override
+  void update(double dt) {
+    position.x += circleDirectionX * circleSpeed * dt;
+    position.y += circleDirectionY * circleSpeed * dt;
+    super.update(dt);
+  }
+
+  @override
+  FutureOr<void> onLoad() {
+    screenWidth = MediaQueryData.fromView(window).size.width;
+    screenHeight = MediaQueryData.fromView(window).size.height;
+
+    circleDirectionX = random.nextInt(2) == 0 ? -1 : 1;
+    circleDirectionY = random.nextInt(2) == 0 ? -1 : 1;
+
+    position =
+        Vector2(random.nextDouble() * (screenWidth - circleWidth), random.nextDouble() * (screenHeight - circleHeigth));
+    size = Vector2(circleWidth, circleHeigth);
+
+    hitBox.paint.color = BasicPalette.green.color;
+    hitBox.renderShape = true;
+
+    add(hitBox);
+    return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is ScreenHitbox) {
+      if (intersectionPoints.first[1] <= 0) {
+        // top
+        circleDirectionX = random.nextInt(2) == 0 ? -1 : 1;
+        circleDirectionY *= -1;
+      } else if (intersectionPoints.first[1] >= screenWidth) {
+        // bottom
+        circleDirectionX = random.nextInt(2) == 0 ? -1 : 1;
+        circleDirectionY *= -1;
+      } else if (intersectionPoints.first[0] <= 0) {
+        // left
+        circleDirectionX *= -1;
+        circleDirectionY = random.nextInt(2) == 0 ? -1 : 1;
+      } else if (intersectionPoints.first[0] >= screenHeight) {
+        // right
+        circleDirectionX *= -1;
+        circleDirectionY = random.nextInt(2) == 0 ? -1 : 1;
+      } else {
+        circleDirectionX *= -1;
+        circleDirectionY *= -1;
+      }
+    }
+
+    hitBox.paint.color = ColorExtension.random();
+    super.onCollision(intersectionPoints, other);
+  }
+}
--- pruebasflame/lib/main.dart
+++ pruebasflame/lib/main.dart
@@ -1,15 +1,22 @@
+import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:pruebasflame/components/circle_position_component.dart';
 import 'package:pruebasflame/components/player_sprite_sheet_component.dart';
 
-class MyGame extends FlameGame with KeyboardEvents, HasTappables, HasKeyboardHandlerComponents {
+class MyGame extends FlameGame with KeyboardEvents, HasTappables, HasKeyboardHandlerComponents, HasCollisionDetection {
+  // Habilitar los que se usan en cada componente tras el
   // HasTappables permite manejar los eventos de toque mediante componentes
   // HasKeyboardHandlerComponents manejar los eventos de teclado mediante componentes
 
   @override
   Future<void> onLoad() async {
+    add(ScreenHitbox()); // HitBox en los bordes de la pantalla
     add(PlayerSpriteSheetComponent());
+    add(CirclePositionComponent());
+    add(CirclePositionComponent());
+    add(CirclePositionComponent());
     return super.onLoad();
   }
 }
